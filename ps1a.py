eecf3ef20f82b557b4a00f44c7b0d0e86a9baf66
Ari Mendoza <blue.tardis91@gmail.com>
2020-10-28 17:43:05 -0600

    unfinished ps1a

---
+++ ps1a.py
@@ -0,0 +1,12 @@
+import math
+annual_salary = input("enter your annual salary:")
+portion_saved = input("enter the percent of your salary to save, as a decimal:")
+total_cost = input("enter the cost of your dream home:")
+portion_down_payment = .25
+current_savings = 0
+portion_down_payment = float(portion_down_payment)
+current_savings = int(current_savings)
+annual_salary = int(annual_salary)
+portion_saved = float(portion_saved)
+total_cost = int(total_cost)
+print(portion_down_payment)
